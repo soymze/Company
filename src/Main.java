74f0cb66aeeb13588893e556b661446ad5822b84
soymze <soylmze@gmail.com>
2024-02-12 21:11:32 +0300

    Company

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee slave = new Employee("noOne", 500,99,1897);
+
+
+        slave.raiseSalary();
+        slave.bonus();
+        slave.tax();
+        slave.toString();
     }
 }
